a5ffa61e166ccefd35724e652c1c8dd8df88bfc3
James Mulligan <james.mulligan@sap.com>
2020-03-06 10:54:00 +0000

    Remove console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,7 @@
-console.log("Client-side JS file is loaded");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
-
-//messageOne.textContent = "from JS";
 
 weatherForm.addEventListener("submit", e => {
   e.preventDefault();
